9daedcf93efd86b33e09f5e6e0a45f780284f1f7
WilliamDoan17 <144507238+WilliamDoan17@users.noreply.github.com>
2024-07-19 05:10:19 +0700

    Update Level 3 - mq7.c

--- Level 3 - MQ7.c
+++ Level 3 - MQ7.c
@@ -1,8 +1,27 @@
+/***************************************************************************//**
+ * @file
+ * @brief Top level application functions
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
 
+/***************************************************************************//**
+ * Initialize application.
+ ******************************************************************************/
 #include "sl_status.h"
 #include "sl_sleeptimer.h"
 #include "app_log.h"
-
+#include "math.h";
 #include "mikroe_mq7.h"
 
 #define READING_INTERVAL_MSEC 3000
@@ -26,25 +45,34 @@ void app_init(void)
   }
 }
 
+double voltage_adc(uint16_t adc) {
+	return ((double) adc) / 1024 * 5;
+}
+
+double sensor_resistance(uint16_t adc) {
+	return (double) (5 * 10 / voltage_adc(adc) - 10);
+}
+
+double ratio(uint16_t adc) {
+	return (double) (sensor_resistance(adc) / 15.605);
+}
+
+double ppm(uint16_t adc) {
+	return (double) (0.35 * pow(ratio(adc), -1.7));
+}
+
 /***************************************************************************//**
  * App ticking function.
  ******************************************************************************/
 void app_process_action(void)
 {
-  float value;
   uint16_t data_out;
 
   if (timer_is_expire == true) {
     timer_is_expire = false;
 
-    if (mikroe_mq7_read_an_pin_voltage(&value) == SL_STATUS_OK) {
-      app_log("AN Voltage: %f V\n", value);
-    } else {
-      app_log("Fail to read\n");
-    }
-
     if (mikroe_mq7_read_an_pin_value(&data_out) == SL_STATUS_OK) {
-      app_log("ADC Value: %d\n", data_out);
+      app_log("PPM value: %.2f\n", ppm(data_out));
     } else {
       app_log("Fail to read\n");
     }
