4db9e889441ea6302c8aca97c5dda5d2fbc1f5c5
WilliamDoan17 <144507238+WilliamDoan17@users.noreply.github.com>
2024-07-19 02:39:17 +0700

    Add files via upload

---
+++ Level 1.c
@@ -0,0 +1,44 @@
+#include <app_log.h>
+#include <em_gpio.h>
+#include <ustimer.h>
+#include <string.h>
+
+// Dev. by: Phong
+
+char DHT11Pin[] = "D3";
+
+void pinMode(char pin[], char mode[]) { // two modes: input and output
+    if (!(strcmp(mode, "in"))) {
+        GPIO_PinModeSet(pin[0] - 'A', pin[1] - '0', gpioModeInput, 0);
+    } else if (!(strcmp(mode, "out"))) {
+        GPIO_PinModeSet(pin[0] - 'A', pin[1] - '0', gpioModePushPull, 0);
+    }
+}
+
+void pinWrite(char pin[], char val[]) {
+    if (!(strcmp(val, "hi"))) {
+        GPIO_PinOutSet(pin[0] - 'A', pin[1] - '0');
+    } else if (!(strcmp(val, "lo"))) {
+        GPIO_PinOutClear(pin[0] - 'A', pin[1] - '0');
+    } else if (!(strcmp(val, "toggle"))) {
+        GPIO_PinOutToggle(pin[0] - 'A', pin[1] - '0');
+    }
+}
+
+uint32_t pinRead(char pin[]) {
+    return GPIO_PinInGet(pin[0] - 'A', pin[1] - '0');
+}
+
+void delay(uint32_t usTime) {
+    USTIMER_Delay(usTime);
+}
+
+void app_init(void) {
+    USTIMER_Init();
+    pinMode("D3", "output");
+}
+
+void app_process_action(void) {
+    pinWrite("D3", "hi");
+    delay(1000000);
+}
